e447ef40a2f846214fdc4c202d180b778d49206b
Lola <lmariemeehan@gmail.com>
2020-06-24 22:17:28 -0700

    Learning about actions with redux and added both increment and decrement counters as an example

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-//STORE -> GLOBALIZED STATE (LIKE A DB) THAT ALL COMPONENTS CAN GRAB DATA FROM
+//STORE -> GLOBALIZED STATE (LIKE A DB) THAT ALL COMPONENTS CAN GRAB DATA FROM. It's read only I believe.
 
-//ACTION INCREMENT
+//ACTION INCREMENT - This just describes what you want to do. It's a simple function that returns an object.
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () {
+  return {
+    type: 'DECREMENT'
+  }
+}
 
 //REDUCER
 
