d96d543f311e62af9d416217d557255a86c14435
Lola <lmariemeehan@gmail.com>
2020-06-27 22:46:10 -0700

    added more notes and technically done with adding all the steps of redux

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 
+// BIG PICTURE OVERVIEW - HOW IT WORKS - The dispatcher dispatches whatever action you tell it to. The action is just a function that returns a named aka "type" object. Then based on the name the reducer decides what it needs to do and returns a piece of state which then gets pushed to updating the store.
+
 //STORE -> GLOBALIZED STATE THAT ALL COMPONENTS CAN GRAB DATA FROM. It's read only I believe. In the past, you would create a state that would belong to a specific component. But with redux, the state is 'globalized' and is called the 'store' 
 
 //ACTION INCREMENT - This just describes what you want to do. It's a simple function that returns an object.
@@ -33,11 +35,12 @@ const counter = (state = 0, action) => {
 //Now this is the actual store. Nothing fancy. The store has a "createStore" function that gets a reducer passed in to it. See below.
 let store = createStore(counter);
 
-//Display it in the console
+//Display it in the console. This is not mandatory at all, just used for checking and debugging.
 store.subscribe(() => console.log(store.getState()))
 
 //DISPATCH - This is the executioner. It sends the action to the reducer, then the reducer checks what it needs to do, then the store gets updated.
 store.dispatch(increment())
+store.dispatch(decrement())
 
 ReactDOM.render(
   <React.StrictMode>
